fff258b13c84f89b6d4b80699f1a441687da5bc4
Shaul Lavochkin <shaul919191@gmail.com>
2023-11-01 18:18:39 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import { useResource } from './hooks/useResource'
+
+const App = () => {
+	const [counter, setCounter] = useState<number>(1)
+	const fetcher = () =>
+		fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`).then(res =>
+			res.json()
+		)
+	const [data, { refetch }] = useResource(fetcher)
+
+	const handleRefetch = () => {
+		setCounter(counter + 1)
+
+		refetch()
+	}
+
+	return (
+		<div>
+			<pre className="data-container">
+				{!data.latest && data.isLoading
+					? 'Loading...'
+					: JSON.stringify(data(), null, 2)}
+				{data.error && <div>Error: {data.error}</div>}
+			</pre>
+
+			<button onClick={handleRefetch}>Refetch</button>
+		</div>
+	)
+}
+
+export default App
---
+++ src/hooks/useResource.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+
+type Resource<T> = {
+	(): T | undefined
+	isLoading: boolean
+	error: string | null
+	latest: T | undefined
+}
+
+type ResourceControls<T> = {
+	refetch: () => void
+	mutate: (value: T) => void
+}
+
+export const useResource = <T,>(
+	fetcher: () => Promise<T>
+): [Resource<T>, ResourceControls<T>] => {
+	const [data, setData] = useState<T | undefined>(undefined)
+	const [latest, setLatest] = useState<T | undefined>(undefined)
+	const [state, setState] = useState<
+		'unresolved' | 'pending' | 'ready' | 'refreshing' | 'errored'
+	>('unresolved')
+	const [error, setError] = useState<string | null>(null)
+
+	const refetch = async () => {
+		const currentState = data ? 'refreshing' : 'pending'
+		setState(currentState)
+		try {
+			const newData = await fetcher()
+			setLatest(data)
+			setData(newData)
+			setState('ready')
+		} catch (err) {
+			setState('errored')
+			if (err instanceof Error) {
+				setError(err.message)
+			} else {
+				setError('An unknown error occurred')
+			}
+		}
+	}
+
+	const mutate = (value: T) => {
+		setData(value)
+		setLatest(data)
+	}
+
+	const resource: Resource<T> = () => data
+	resource.isLoading = state === 'pending' || state === 'refreshing'
+	resource.error = error
+	resource.latest = latest
+
+	useEffect(() => {
+		refetch()
+	}, [])
+
+	return [
+		resource,
+		{
+			refetch,
+			mutate,
+		},
+	]
+}
